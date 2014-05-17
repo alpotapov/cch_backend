58823301506c2142cb1f60268e4bf0a0d2f00de7
AlPotapov <herr.potapov@googlemail.com>
2014-05-17 12:20:01 +0200

    database configuration

--- cch_backend/settings.py
+++ cch_backend/settings.py
@@ -56,10 +56,15 @@ WSGI_APPLICATION = 'cch_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'cch_database',                      # Or path to database file if using sqlite3.
+        'USER': 'cch_user',
+        'PASSWORD': 'cch_user_password',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
