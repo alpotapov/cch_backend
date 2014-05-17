d927393ced96300ba06ab41a5b254102523b7887
Patrick Buergin <buergin@in.tum.de>
2014-05-17 15:17:11 +0200

    added extract_lat_lon_from_map that returns a sorted list of (lat,long,id)

--- restservice/gas_stations_adapter.py
+++ restservice/gas_stations_adapter.py
@@ -10,3 +10,12 @@ def get_current_gas_station_map():
             gas_station_map[gas_station.get('id')] = gas_station
 
         return gas_station_map
+
+def extract_lat_lon_from_map(gas_station_map):
+    extract = lambda key: (
+            gas_station_map[key].get('latitude'), 
+            gas_station_map[key].get('longitude'),
+            key
+        )
+    return sorted(map(extract, gas_station_map))
+
