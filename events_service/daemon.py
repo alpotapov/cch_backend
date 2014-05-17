21a323144524233218a8d64f0d038c024ff12086
AlPotapov <herr.potapov@googlemail.com>
2014-05-18 00:40:33 +0200

    minor changes

--- events_service/daemon.py
+++ events_service/daemon.py
@@ -3,13 +3,20 @@ import time
 
 processed_entries = []
 
+class EventHandler():
+    def __init__(self):
+        pass
+
+    def process(self, entry):
+        # pattern 1: stop at gas station
+        if not entry['DIO_IGNITION']:
+            
+
 while True:
-    with open('bla.json', 'r') as txt:
-        test = json.load(txt)
-        for entry in test:
-            if entry['id'] in processed_entries:
-                continue
-            print entry['name']
+    with open('engine_stops.json', 'r') as txt:
+        entries = json.load(txt)
+        for entry in entries:
+             entry['name']
             processed_entries.append(entry['id'])
 
     print('+++++++')
