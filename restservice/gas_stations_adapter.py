127b5984dc3e8be1f61500b936cbd63a05edfce1
Patrick Buergin <buergin@in.tum.de>
2014-05-17 14:29:10 +0200

    added the json dump of the gas station data and a basic parser

---
+++ restservice/gas_stations_adapter.py
@@ -0,0 +1,12 @@
+import json
+
+def get_current_gas_station_map():
+    with open('gas_stations_data.json', 'r') as json_txt:
+        json_dict = json.load(json_txt)
+        gas_stations = json_dict.get('gas_stations')
+
+        gas_station_map = {}
+        for gas_station in gas_stations:
+            gas_station_map[gas_station.get('id')] = gas_station
+
+        return gas_station_map
