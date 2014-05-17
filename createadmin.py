ec1e90a8c96aa6b8a8942441bce0ea77620488e2
Daniel Sel <aresfx2@gmail.com>
2014-05-17 18:24:29 +0200

    asd

--- createadmin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-if User.objects.count() == 0:
-    admin = User.objects.create_user('cch_user', '', 'cch_user_password')
-    admin.is_superuser = True
-    admin.is_staff = True
-    admin.save()
