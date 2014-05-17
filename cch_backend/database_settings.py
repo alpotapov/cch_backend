2c54c86f3251112f1997c76be322ee830e1021a4
Daniel Sel <aresfx2@gmail.com>
2014-05-17 16:30:17 +0200

    database settings update

--- cch_backend/database_settings.py
+++ cch_backend/database_settings.py
@@ -1,10 +1,10 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'cch_database',                      # Or path to database file if using sqlite3.
-        'USER': 'cch_user',
-        'PASSWORD': 'cch_user_password',
-        'HOST': 'localhost',
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'smartgasdb',                      # Or path to database file if using sqlite3.
+        'USER': 'smartgas_user',
+        'PASSWORD': 'fqxn9*GbBs8@:yL',
+        'HOST': 'smartgasrds.cl6k8oli4quw.eu-west-1.rds.amazonaws.com',
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
